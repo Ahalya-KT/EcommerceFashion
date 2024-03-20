c9ec996ba99d668479676cbf0835ca1375a31aa2
Ahalya-KT <ahalyakt002@gmail.com>
2024-03-20 23:28:50 +0530

    ecommerce website

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <h1 className='font-semibold text-teal-500'>Ecommerce-website</h1>
+      </div>
+      
+     
+    </>
+  )
+}
+
+export default App
