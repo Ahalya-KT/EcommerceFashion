d173221a8baa0452a50ab18c378abcbf7b94ce34
Ahalya-KT <ahalyakt002@gmail.com>
2024-03-24 00:38:59 +0530

    signUp login

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { Switch, ThemeProvider } from '@material-tailwind/react'
+import { ThemeProvider } from '@material-tailwind/react'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
