b8643d26989992e3c7f6c6c3d3a411cbcdf6e3f2
Ahalya-KT <ahalyakt002@gmail.com>
2024-03-28 23:37:36 +0530

    responsive navbar

--- src/Page/Home.tsx
+++ src/Page/Home.tsx
@@ -1,6 +1,7 @@
 
 import Navbar from '../component/Navbar';
-import img1 from "../images/img_1.png";
+// import img1 from "../images/img_1.png";
+
 
 
 
@@ -11,15 +12,21 @@ const Home = () => {
       <Navbar/>
       </div>
     
-     <div className='py-16  md:absolute w-full'>
+     {/* <div className='py-16  md:absolute w-full '>
       <img src={img1} className='w-full object-cover overflow-hidden' />
-      <p className='md:relative bottom-96 text-right right-36 text-5xl text-white uppercase font-bold'>stylist picks beat 
-      the heat</p>
+      <div className='md:relative bottom-96 text-right right-36 text-5xl  leading-snug'>
+      <p  className='text-white uppercase font-bold'>stylist picks beat 
+      </p>
+     <p className='text-white uppercase font-bold'>the heat</p>
+
+     <button className='text-sm text-white border-4 p-2'>SHOP NOW</button>
+        </div>
+      
       <div>
        
       </div>
      </div>
-    
+     */}
     </div>
   )
 }
--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,7 +1,10 @@
 import { GiLargeDress } from "react-icons/gi";
-import { FaRegUser } from "react-icons/fa6";
-import { MdOutlineShoppingBag } from "react-icons/md";
+// import { FaRegUser } from "react-icons/fa6";
+// import { MdOutlineShoppingBag } from "react-icons/md";
 import { IoIosMenu } from "react-icons/io";
+import { RxCross2 } from "react-icons/rx";
+import { useState } from "react";
+
 
 const Navbar = () => {
   let Links = [
@@ -9,30 +12,41 @@ const Navbar = () => {
     { name: "ABOUT", link: "/" },
     { name: "CONTACT US", link: "/" },
   ];
+  const[isOpen,setOpen]=useState(false)
+
   return (
     <div className=" w-full bg-white">
-      <div className="md:px-10 py-6 px-7 md:flex fixed top-0 w-full ">
+      <div className="md:px-10 py-6 px-7 md:flex fixed top-0 left-0 w-full ">
         <div className="flex text-2xl cursor-pointer items-center gap-1">
           <GiLargeDress className="w-7 h-7 text-pink-200" />
           <span className="font-bold">NorthStar</span>
         </div>
+        {/* menu icon */}
+        <div onClick={()=>setOpen(!isOpen)} className="absolute right-8 top-6 md:hidden">
+          {
+          isOpen ?  <IoIosMenu /> :   <RxCross2 /> 
+        }
+        </div>
+       
+
         {/* nav link */}
-        <div className="flex justify-between w-full">
-          <div>
-            <ul className="md:flex pl-9 md:pl-0">
+        <div className="flex ">
+       
+            <ul className={`md:flex pl-9 md:pl-0 md:items-center md:pb-0 pb-12 absolute w-full transition-all duration-500 ease-in ${isOpen ? 'top-8' : 'top-[-490]'}`}>
               {Links.map((link) => (
-                <li className="my-7 md:my-0 md:ml-12 ml-3 text-sm font-semibold ">
+                <li className="my-7 md:my-0 md:ml-12 ml-3 text-sm font-semibold  ">
                   <a href="/">{link.name}</a>
                 </li>
               ))}
             </ul>
-          </div>
-          
-          <div className="flex gap-3">
+
+            {/* <div className="flex gap-3 md:static">
             <FaRegUser />
             <MdOutlineShoppingBag />
             <IoIosMenu />
-          </div>
+          </div> */}
+          
+        
         </div>
       </div>
     </div>
