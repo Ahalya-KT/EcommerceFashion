b92cf1f319af96a8782d81a6d2a4101f7061fbda
Ahalya-KT <ahalyakt002@gmail.com>
2024-03-29 11:42:08 +0530

    responsive home page

--- src/Page/Home.tsx
+++ src/Page/Home.tsx
@@ -1,34 +1,26 @@
-
-import Navbar from '../component/Navbar';
-// import img1 from "../images/img_1.png";
-
-
-
+import Navbar from "../component/Navbar";
+import img1 from "../images/img_1.png";
 
 const Home = () => {
   return (
     <div>
-      <div className='bg-white'>
-      <Navbar/>
+      <div className="bg-white">
+        <Navbar />
       </div>
-    
-     {/* <div className='py-16  md:absolute w-full '>
-      <img src={img1} className='w-full object-cover overflow-hidden' />
-      <div className='md:relative bottom-96 text-right right-36 text-5xl  leading-snug'>
-      <p  className='text-white uppercase font-bold'>stylist picks beat 
-      </p>
-     <p className='text-white uppercase font-bold'>the heat</p>
 
-     <button className='text-sm text-white border-4 p-2'>SHOP NOW</button>
+      <div className="py-16  md:absolute w-full ">
+        <img src={img1} className="w-full object-cover overflow-hidden" />
+        <div className="relative md:bottom-96 text-right md:right-36 md:text-5xl  leading-snug bottom-32 right-11 ">
+          <p className="text-white uppercase font-bold">stylist picks beat</p>
+          <p className="text-white uppercase font-bold">the heat</p>
+
+          <button className="text-sm text-white border-4 md:p-2">SHOP NOW</button>
         </div>
-      
-      <div>
+
        
       </div>
-     </div>
-     */}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -5,14 +5,13 @@ import { IoIosMenu } from "react-icons/io";
 import { RxCross2 } from "react-icons/rx";
 import { useState } from "react";
 
-
 const Navbar = () => {
   let Links = [
     { name: "HOME", link: "/" },
     { name: "ABOUT", link: "/" },
     { name: "CONTACT US", link: "/" },
   ];
-  const[isOpen,setOpen]=useState(false)
+  const [isOpen, setOpen] = useState(false);
 
   return (
     <div className=" w-full bg-white">
@@ -22,31 +21,32 @@ const Navbar = () => {
           <span className="font-bold">NorthStar</span>
         </div>
         {/* menu icon */}
-        <div onClick={()=>setOpen(!isOpen)} className="absolute right-8 top-6 md:hidden">
-          {
-          isOpen ?  <IoIosMenu /> :   <RxCross2 /> 
-        }
+        <div
+          onClick={() => setOpen(!isOpen)}
+          className="absolute right-8 top-6 md:hidden"
+        >
+          {isOpen ?  <RxCross2 /> :<IoIosMenu />}
         </div>
-       
-
+        
         {/* nav link */}
         <div className="flex ">
-       
-            <ul className={`md:flex pl-9 md:pl-0 md:items-center md:pb-0 pb-12 absolute w-full transition-all duration-500 ease-in ${isOpen ? 'top-8' : 'top-[-490]'}`}>
-              {Links.map((link) => (
-                <li className="my-7 md:my-0 md:ml-12 ml-3 text-sm font-semibold  ">
-                  <a href="/">{link.name}</a>
-                </li>
-              ))}
-            </ul>
+          <ul
+            className={`md:flex pl-9 md:pl-0 md:items-center bg-white md:static md:pb-0 pb-12 absolute w-full transition-all duration-500 ease-in ${
+              isOpen ? "top-16" : "top-[-490px] " 
+            }`}
+          >
+            {Links.map((link) => (
+              <li className="my-7 md:my-0 md:ml-12 ml-3 text-sm font-semibold  ">
+                <a href="/">{link.name}</a>
+              </li>
+            ))}
+          </ul>
 
-            {/* <div className="flex gap-3 md:static">
+          {/* <div className="flex gap-3 md:static">
             <FaRegUser />
             <MdOutlineShoppingBag />
             <IoIosMenu />
           </div> */}
-          
-        
         </div>
       </div>
     </div>
